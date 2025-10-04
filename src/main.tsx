7f1f47cee258be0fb28218a09caf837be8e2bb9f
rafiqmia65 <rafiqdeveloper4465@gmail.com>
2025-10-05 04:16:17 +0600

    Shadcn Theme setup done

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { ThemeProvider } from "./components/ThemeProvider.tsx";
+import { ThemeProvider } from "./components/theme-provider.tsx";
 import { RouterProvider } from "react-router";
 import { router } from "./routes/routes.tsx";
 
